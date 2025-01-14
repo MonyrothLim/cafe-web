4d487785444f045d9672743fef5ba86e7142ffd1
Monyroth Lim <vlogmr905@gmail.com>
2025-01-14 22:21:46 +0700

    finish homepage

---
+++ home.js
@@ -0,0 +1,25 @@
+let slideIndex = 0;
+
+
+function showSlides() {
+    let slides = document.querySelectorAll(".slide");
+    
+
+    slides.forEach(slide => slide.style.display = "none");
+
+
+    slideIndex++;
+
+
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+
+
+    slides[slideIndex].style.display = "block";
+
+    setTimeout(showSlides, 5000);
+}
+
+
+window.onload = showSlides;
