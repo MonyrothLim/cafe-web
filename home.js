667fd42b5e0e8c71f3a7f14a3172c045d381d2e4
Monyroth Lim <vlogmr905@gmail.com>
2025-01-14 22:51:04 +0700

    homepage done

--- home.js
+++ home.js
@@ -1,25 +1,23 @@
 let slideIndex = 0;
-
-
-function showSlides() {
+showSlides(slideIndex);
+// Function to move slides
+function moveSlide(step) {
+    slideIndex += step;
+    showSlides(slideIndex);
+}
+// Function to show slides based on the index
+function showSlides(n) {
     let slides = document.querySelectorAll(".slide");
-    
 
-    slides.forEach(slide => slide.style.display = "none");
-
-
-    slideIndex++;
-
-
-    if (slideIndex >= slides.length) {
+    if (n >= slides.length) {
         slideIndex = 0;
     }
 
-
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    // Hide all slides
+    slides.forEach(slide => slide.style.display = "none");
+    // Show the current slide
     slides[slideIndex].style.display = "block";
-
-    setTimeout(showSlides, 5000);
 }
-
-
-window.onload = showSlides;
